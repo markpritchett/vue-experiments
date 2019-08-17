1f995d65b75dad3e7268ca7fb0e5e36347a16562
mark pritchett <markpritchett@hotmail.co.uk>
2019-08-17 16:24:14 +0100

    chore: add SVG icon example
    credits:
    https://github.com/sdras/vue-sample-svg-icons/
    https://icomoon.io/#preview-free

--- 01/client/src/views/Home.vue
+++ 01/client/src/views/Home.vue
@@ -8,16 +8,46 @@
         <li :key="value" v-for="value in values">{{value}}</li>
       </ul>
     </section>
+    <section>
+      <h3>Icons</h3>
+      <IconBase icon-name="calendar">
+        <IconCalendar />
+      </IconBase>
+      <IconBase icon-name="calendar" width="24" height="24">
+        <IconCalendar />
+      </IconBase>
+      <IconBase icon-name="calendar" width="36" height="36">
+        <IconCalendar />
+      </IconBase>
+      <IconBase icon-name="calendar" width="48" height="48">
+        <IconCalendar />
+      </IconBase>
+      <IconBase icon-name="calendar" width="60" height="60">
+        <IconCalendar />
+      </IconBase>
+      <IconBase
+        icon-name="calendar"
+        width="72"
+        height="72"
+        icon-color="#42b983"
+      >
+        <IconCalendar />
+      </IconBase>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import IconBase from "@/components/Icons/IconBase.vue";
+import IconCalendar from "@/components/Icons/IconCalendar.vue";
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    IconBase,
+    IconCalendar
   }
 })
 export default class Home extends Vue {
@@ -29,3 +59,8 @@ export default class Home extends Vue {
   }
 }
 </script>
+<style scoped>
+section svg {
+  vertical-align: middle;
+}
+</style>
