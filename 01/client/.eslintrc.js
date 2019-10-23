97b135598f80d98f91d3788da356bef00c106784
mark pritchett <markpritchett@hotmail.co.uk>
2019-10-23 06:06:52 +0100

    chore: add lint rules

--- 01/client/.eslintrc.js
+++ 01/client/.eslintrc.js
@@ -6,7 +6,10 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prefer-const": "error",
+    "no-var": "error",
+    "object-shorthand": "error"
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
