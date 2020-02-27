cce762d2f5a36dfcae1acbe699d4ecf0a8c30636
Mark Pritchett <markpritchett@hotmail.co.uk>
2020-02-27 21:17:30 +0000

    chore: add cypress e2e testing

--- 01/client/.eslintrc.js
+++ 01/client/.eslintrc.js
@@ -5,17 +5,14 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    'eslint:recommended'
   ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "prefer-const": "error",
-    "no-var": "error",
-    "object-shorthand": "error"
-  },
   parserOptions: {
     parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   overrides: [
     {
--- 01/client/tests/e2e/specs/test.js
+++ 01/client/tests/e2e/specs/test.js
@@ -1,8 +1,13 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
-    cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
-  });
-});
+describe('My First Test', () => {
+  it('Visits the app root url', () => {
+    cy.visit('/')
+    cy.get('#Username').type('alice')
+    cy.get('#Password').type('alice')
+    cy.get('#login').click()
+    cy.get('#giveAppAccess').then(el => el.click())
+    // cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.get('nav').contains('Alice Smith')
+  })
+})
